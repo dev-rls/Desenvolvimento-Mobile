e71f3c95d18e3b14cad31baaaf00f23234ead085
Raquel Santos <raquellemos1993@hotmail.com>
2025-04-09 17:38:41 -0400

    Aula 09-04-25
    Prof Mauricio

--- Mobile/projetoteste/App.js
+++ Mobile/projetoteste/App.js
@@ -1,15 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native'; // Corrigido "Buttom" para "Button"
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
-export default function App() {
+export default function App() {// Mesmo que def
+  const handlePress = () => { 
+    alert("Você clicou! Parabéns");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Abra o App.js para começar a trabalhar no seu aplicativo!</Text>
+      <Text>Raquel</Text> <br></br>
+      <Image
+        style={styles.image} 
+        source={require('./morango.jpg')} 
+      /><br/>
+      <Button 
+        title="Pressione aqui" 
+        onPress={handlePress} 
+      /> <br/>
       <FontAwesome5 name="cube" size={24} color="black" />
-      <img>
-      
-      </img>
-
     </View>
   );
 }
@@ -21,4 +29,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 300, // Define largura da imagem
+    height: 300, // Define altura da imagem
+  },
 });
+
+
+
