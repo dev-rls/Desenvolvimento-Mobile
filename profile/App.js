467621c897d9ec480c3288b0240b2c1ac314418e
Raquel Santos <raquellemos1993@hotmail.com>
2025-04-16 14:12:56 -0400

    ...
    ...

--- profile/App.js
+++ profile/App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar, Image, ScrollView } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+ 
+function MinhasCredenciais({ githubUser }) {
+  return (
+    <View style={{ flexDirection: 'row', padding: 15, alignItems: 'center' }}>
+      <Image
+        style={{
+          marginRight: 15,
+          width: 40,
+          height: 40,
+          borderRadius: 20,
+        }}
+        source={{
+          uri: `https://github.com/${githubUser}.png`,
+        }}
+      />
+     
+      <Image
+        style={{
+          width: 200,
+          height: 200,
+        }}
+        source={{
+          uri: `https://github.com/${githubUser}.png`,
+        }}
+      />
+ 
+      <AntDesign name="hearto" size={24} color="black" />
+    </View>
+  );
+}
+ 
 export default function App() {
+ 
+  const githubUsers = ['dev-rls','HisanoEdu', 'jamilyGomes'];
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView>
+        {githubUsers.map((githubUser) => (
+          <MinhasCredenciais key={githubUser} githubUser={githubUser} />
+        ))}
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'aliceblue',
   },
 });
